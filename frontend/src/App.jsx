23a6b202c7b1b4e477e335d2347c30513e7f3295
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-01-20 23:42:20 +0530

    added a signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import Main from './components/Main'
 import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/LoginPage'
+import SignUpPage from './pages/SignUpPage'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <LandingPage />
+      <SignUpPage />
     </div>
   )
 }
---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,10 @@
+export default function SignUpPage(){
+    return (
+        <main className="w-screen h-screen flex flex-row">
+            <section className="w-6/12 bg-white"></section>
+            <section className="w-6/12 bg-violet-500 flex justify-center items-center">
+                <img src="https://o.remove.bg/downloads/0e42768b-8f36-4653-8653-d71179713325/pngtree-purple-office-ui-registration-illustration-png-image_1691877-removebg-preview.png" alt="sign-up-image" className="w-10/12"/>
+            </section>
+        </main>
+    )
+}
