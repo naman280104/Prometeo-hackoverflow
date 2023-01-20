d8e6271c88ad6e132562660d6ee5e8e118acb88b
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-01-20 22:00:53 +0530

    added the login page and finished the home page

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import NavBar from "../components/NavBar";
+
 
 export default function LandingPage() {
   return (
-    <div className="underline">
-      <h1>Landing Page</h1>
-    </div>
+    <>
+    <NavBar />
+    <main className="flex width-full justify-between h-full">
+        <section className="width-6/12 h-screen flex flex-col justify-center p-20">
+            <h1 className="text-6xl font-bold">Build your Resume for Free</h1>
+            <h6 className="text-3xl">Create your resume easily with our free builder and professional templates.</h6>
+            <button className="w-4/12 my-4 py-4 text-2xl bg-violet-500 text-white font-semibold">Create My Resume</button>
+        </section> 
+        <section className="width-6/12 h-screen flex justify-center align-center">
+            <img src="https://www.resume.com/static/fb4669c69e1a8f8a16eb0204c1535713/36490/first-screen.webp" alt="resume-image" />
+        </section> 
+    </main>
+    </>
   );
 }
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    color: {
+      'buttonColor': '#440ccc'
+    }
   },
   plugins: [],
 }
