514bc6e5712b922bc8341b48d392f63c7270dcdc
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-01-20 13:13:04 +0530

    added models for the user

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,20 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 8000;
-require('dotenv/require');
+require('dotenv/config');
 
+
+// connecting with the database
+const uri = process.env.DB_CONNECTION
 const connect = async() =>{
-    await mongoose.connect()
+    try {
+        await mongoose.connect(uri)
+        console.log("DB connected")
+    } catch (err) {
+        console.log(err);
+    }
 }
+
+connect();
 
 app.listen(PORT, () => console.log("THE SERVER IS LISTENING"))
