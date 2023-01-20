35e1a39162b7088bf92d657f7223c270b1b18c8c
Anadi Sharma <sharma.130@iitj.ac.in>
2023-01-20 23:10:07 +0530

    Resume page completed

--- frontend/src/components/BasicInfo.jsx
+++ frontend/src/components/BasicInfo.jsx
@@ -4,8 +4,11 @@ export default function BasicInfo() {
   return (
     <div className="prsn">
       <center><input className='ipfname' type="text"placeholder='Enter Your Name' /></center>
-      <center><input className='ipfph' type="tel" name="" id="" placeholder='Enter Phone Number'/>|
-      <input className='ipfem' type="email" name="" id="" placeholder='Enter your email id'/></center>
+      <center>
+        <input className='ipfem' type="email" name="" id="" placeholder='Enter your email id'/>|
+        <input className='ipfph' type="tel" name="" id="" placeholder='Enter Phone Number'/>
+      </center>
+      <hr style={{margin: "0px 0px 15px 0px"}} />
     </div> 
   )
 }
