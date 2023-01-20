3d0f0b3b0931a221db791e55ca62469abc8c6c45
Naman Goyal <naman280104@gmail.com>
2023-01-20 21:21:39 +0530

    Education popup added

--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
+import EducationPopup from './EducationPopup'
+import { useState } from 'react'
+export default function Education(props){
+  const [popup,setPopup] = useState(false)
+  const openit=()=>{
+    // document.getElementsByClassName("App")[0].style.opacity = "0.2";
+    // document.getElementsByClassName("popup-education")[0].style.opacity = "1";
+    // console.log(document.getElementsByClassName("popup-education")[0]);
+    setPopup(true)
+    // document.getElementById("popupedu").style.opacity = "1";
 
-export default function Education() {
+  }
   return (
     <div className='education'>
-        <b><input className='ipfh' type="text" placeholder='EDUCATION'/></b>
+        <button className="iphf" onClick={openit}>EDUCATION</button>
         <div>
             <div className="eds">
-                Degree-1
+                School
             </div>
             <div className="desc">
                 Description
             </div>
         </div>
+        <EducationPopup setPopup = {setPopup} popup = {popup}/>
     </div>
   )
 }
