22cc855ab39f19f040204586619d16b46b9d3e83
Kunal Tiwari <tiwari.25@iitj.ac.in>
2023-01-21 01:12:17 +0530

    finished the signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,25 +1,25 @@
 export default function SignUpPage(){
     return (
-        <main className="w-screen h-screen flex flex-row">
+        <main className="w-screen h-screen flex flex-row font-Poppins">
             <section className="w-6/12 bg-white flex justify-center items-center">
-                <ul className="flex flex-col w-8/10">
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS2ueunwcP9xn_du2dRu5L1Xl2biU2lT2LWug2CbRmVA&s" alt="" />
-                    <h1 className="text-3xl my-5">Sign Up</h1>
-                    <div>
+                <ul className="flex flex-col w-10/12">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS2ueunwcP9xn_du2dRu5L1Xl2biU2lT2LWug2CbRmVA&s" alt="" className="w-4/12 mx-auto"/>
+                    <h1 className="text-3xl my-5 font-bold">Sign Up</h1>
+                    <div className="my-2 w-full">
                         <h6>Full Name</h6>
-                        <input className="w-full p-2" type="text" placeholder="Full Name"/>
+                        <input className="w-full p-2 border-solid border-gray-300 border" type="text" placeholder="Full Name"/>
                     </div>
-                    <div>
+                    <div className="my-2">
                         <h6>Username</h6>
-                        <input className="w-full p-2" type="text" placeholder="Full Name"/>
+                        <input className="w-full p-2 border border-solid border-gray-300" type="text" placeholder="Full Name"/>
                     </div>
-                    <div>
+                    <div className="my-2">
                         <h6>Email </h6>
-                        <input className="w-full p-2" type="text" placeholder="Full Name"/>
+                        <input className="w-full p-2 border border-solid border-gray-300" type="text" placeholder="Full Name"/>
                     </div>
-                    <div>
+                    <div className="my-2">
                         <h6>Password</h6>
-                        <input className="w-full p-2" type="text" placeholder="Full Name"/>
+                        <input className="w-full p-2 border border-solid border-gray-300" type="text" placeholder="Full Name"/>
                     </div>
 
              
