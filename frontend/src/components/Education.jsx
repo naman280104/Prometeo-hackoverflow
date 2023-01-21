3338dd11fb451d1a4f8c54daacaf1dbe6b7b2b15
Anadi Sharma <sharma.130@iitj.ac.in>
2023-01-21 10:25:13 +0530

    conflicts resolved

--- frontend/src/components/Education.jsx
+++ frontend/src/components/Education.jsx
@@ -4,25 +4,23 @@ import { useState } from 'react'
 export default function Education(props){
   const [popup,setPopup] = useState(false)
   const openit=()=>{
-    // document.getElementsByClassName("App")[0].style.opacity = "0.2";
-    // document.getElementsByClassName("popup-education")[0].style.opacity = "1";
-    // console.log(document.getElementsByClassName("popup-education")[0]);
     setPopup(true)
-    // document.getElementById("popupedu").style.opacity = "1";
-
   }
   return (
-    <div className='education'>
-        <center><button className="ipfh" onClick={openit}>EDUCATION</button></center>
-        <div>
-            <div className="eds">
-                School
-            </div>
-            <div className="desc">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, quaerat.
-            </div>
-        </div>
-        <EducationPopup setPopup = {setPopup} popup = {popup}/>
-    </div>
+    <>
+      <div className='education'>
+          <center><button className="ipfh" onClick={openit}>EDUCATION</button></center>
+          <div>
+              <div className="eds">
+                  Schooling
+              </div>
+              <div className="desc">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, quaerat.
+              </div>
+          </div>
+          <EducationPopup setPopup = {setPopup} popup = {popup}/>
+      </div>
+    </>
+  
   )
 }
--- frontend/src/components/Skills.jsx
+++ frontend/src/components/Skills.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
-
+import React, { Children } from 'react'
+import { useState } from 'react'
 export default function Skills() {
-  return (
-    <div className='Skills'>
-        <center><button className='ipfh'>SKILLS</button></center>
-        <div className='skill'>
-            <div>HTML</div>
-            <div>CSS</div>
-            <div>JS</div>
-            <div>Django</div>
-            <div>Flutter</div>
-        </div>
-    </div>
-  )
+	const [skills, setSkills] = useState([]);
+	function handleKeyDown(e){
+		if (e.key !== 'Enter') return;
+
+		const value = e.target.value;
+		if (!value.trim()) return;
+		
+		setSkills([...skills, value]);
+		e.target.value = '';
+		e.target.placeholder = '';
+	}
+
+	function removeSkill(index){
+		if (index===0){
+			document.getElementsByClassName("tag-input")[0].placeholder = 'Type Some Skills';
+		}
+		setSkills(skills.filter((el, i) => i !==index));
+	}
+	return (
+		<div className='Skills'>
+			<center><button className='ipfh'>SKILLS</button></center>
+			<div className='skill'>
+				{ skills.map((skill, index) => (
+					<div className="skill-item" key={index}>
+						<span className='text'>{skill}</span>
+						<span className='close' onClick={() => removeSkill(index,self)}>&times;</span>
+					</div>
+				))}
+				<input onKeyDown={handleKeyDown} type="text" className='tag-input' placeholder='Type Some Skills' name="" id="" />
+			</div>
+		</div>
+	)
 }
